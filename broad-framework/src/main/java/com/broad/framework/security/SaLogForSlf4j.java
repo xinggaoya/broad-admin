727312d3aa7eb47020b59c54f9df241deec7b41c
XingGao <1032227283@qq.com>
2023-01-03 23:00:41 +0800

    整理

---
+++ broad-framework/src/main/java/com/broad/framework/security/SaLogForSlf4j.java
@@ -0,0 +1,46 @@
+package com.broad.framework.security;
+
+import cn.dev33.satoken.log.SaLog;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: XingGao
+ * @date: 2022/12/22
+ * @description: 将 Sa-Token log 信息转接到 Slf4j
+ */
+@Component
+@Slf4j
+public class SaLogForSlf4j implements SaLog {
+
+    @Override
+    public void trace(String str, Object... args) {
+        log.trace(str, args);
+    }
+
+    @Override
+    public void debug(String str, Object... args) {
+        log.debug(str, args);
+    }
+
+    @Override
+    public void info(String str, Object... args) {
+        log.info(str, args);
+    }
+
+    @Override
+    public void warn(String str, Object... args) {
+        log.trace(str, args);
+    }
+
+    @Override
+    public void error(String str, Object... args) {
+        log.error(str, args);
+    }
+
+    @Override
+    public void fatal(String str, Object... args) {
+        log.error(str, args);
+    }
+}
+
